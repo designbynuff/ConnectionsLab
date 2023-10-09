3e46e00b60b4e106a3e2e3bf1c2a7375efa4a801
designbynuff <info@designbynuff.com>
2023-10-09 15:48:55 -0400

    Placeholder Sketch
    Sketch is working—I had Copilot make some flow fields and tweaked them a bit. However, there's a weird margin issue so the BG image doesn't fully cover the canvas.

--- assignments/project01-metyourself/sketch.js
+++ assignments/project01-metyourself/sketch.js
@@ -5,15 +5,31 @@ function setup() {
   canvas = createCanvas(windowWidth, windowHeight);
   canvas.position(0, 0);
   canvas.style("z-index", "-1");
+
+  background(27, 28, 33);
 }
 
 function draw() {
-  // Draw a random color background
-  let r = random(255);
-  let g = random(255);
-  let b = random(255);
+  // Create Flow Fields using Perlin Noise (I Let Copilot do this for me and tweaked some variables)
+  let xoff = 0;
+  for (let x = 0; x < width; x += 100) {
+    let yoff = 0;
+    for (let y = 0; y < height; y += 100) {
+      let angle = noise(xoff, yoff, frameCount * 0.005) * TWO_PI * 4;
+      let v = p5.Vector.fromAngle(angle);
+      v.setMag(1);
+      stroke(191, 208, 252, 2);
+      push();
+      translate(x, y);
+      rotate(v.heading());
+      line(0, 0, 300, 0);
+      pop();
+      yoff += 0.1;
+    }
+    xoff += 0.1;
+  }
 
-  background(r, g, b)
+
 }
 
 function windowResized() {
